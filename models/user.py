11ecb73734e4dfa26379d83e5d70ff3df038504c
BorjaEst <boressan@outlook.com>
2021-10-27 07:54:56 +0200

    Add user creation_date

--- models/user.py
+++ models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime as dt
+
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.orm import relationship
 
 from . import Base
@@ -8,5 +10,6 @@ class User(Base):
     __tablename__ = 'user_account'
 
     id = Column(Integer, primary_key=True)
+    creation_date = Column(DateTime, default=dt.now())
     name = Column(String(30))
     fullname = Column(String)
