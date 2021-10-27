662de76dcf9152785f49ecaa235e2709715bd97c
BorjaEst <boressan@outlook.com>
2021-10-27 07:59:22 +0200

    Add user name, surname and make unique constraint

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@ from datetime import datetime as dt
 
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import UniqueConstraint
+
 
 from . import Base
 
@@ -11,5 +13,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     creation_date = Column(DateTime, default=dt.now())
-    name = Column(String(30))
-    fullname = Column(String)
+    name = Column(String(30), nullable=False)
+    surname = Column(String(100), nullable=False)
+
+    __table_args__ = (UniqueConstraint('name', 'surname'),)
